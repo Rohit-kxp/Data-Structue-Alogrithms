4838b0662ad92be811097e48b694ab57c4598de8
Rohit kashyap <125393366+RohitXcodes@users.noreply.github.com>
2023-10-12 16:47:06 +0000

    counting by recursion

--- 02_counting.cpp
+++ 02_counting.cpp
@@ -1,3 +1,5 @@
+/*print counting 5 to 1 from recursion 
+
 #include <iostream>
 using namespace std;
 void print(int n){
@@ -10,6 +12,26 @@ void print(int n){
     cout<< n <<" ";
     //recursion call
     print(n-1);
+}
+int main(){
+    print(9);
+    return 0 ;
+}
+
+*/
+
+// print counting 1 to n from recursion 
+
+#include<iostream>
+using namespace std;
+void print(int n){
+    if(n==1){
+        cout<<1;
+        return;
+    }
+    print(n-1);
+    cout<<n ;
+    return ;
 }
 int main(){
     print(9);

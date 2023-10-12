6f73081fc8ce6f660152201a515708acd4dcc346
Rohit kashyap <125393366+RohitXcodes@users.noreply.github.com>
2023-10-12 17:57:22 +0000

    power by recursion

---
+++ 03_power.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int pow(int n){
+    if(n==0){
+        return 1;
+    }
+    int ans = 2 * pow(n-1);
+    return ans;
+}
+int main(){
+    cout<<pow(10)<<endl;
+    return 0 ;
+}
